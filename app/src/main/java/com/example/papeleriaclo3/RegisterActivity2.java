6f095264fb28ef9386b1d79affb4619d14277631
LuisVascoE <luis.vasco@o365.unab.edu.co>
2022-11-18 22:54:05 -0500

    FireBase

--- app/src/main/java/com/example/papeleriaclo3/RegisterActivity2.java
+++ app/src/main/java/com/example/papeleriaclo3/RegisterActivity2.java
@@ -3,12 +3,28 @@ package com.example.papeleriaclo3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity2 extends AppCompatActivity {
 
+    CircleImageView mCircleImageViewback;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        mCircleImageViewback= findViewById(R.id.cirleimageback);
+        mCircleImageViewback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
+
+
