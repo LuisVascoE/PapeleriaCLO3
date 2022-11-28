91b0a6e88689b478c040605a229ef55536297c41
LuisVascoE <luis.vasco@o365.unab.edu.co>
2022-11-28 18:40:42 -0500

    Bottom navigation menu

--- app/src/main/java/com/example/papeleriaclo3/Activities/HomeActivity2.java
+++ app/src/main/java/com/example/papeleriaclo3/Activities/HomeActivity2.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.papeleriaclo3.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity2 extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
     }
+
+
 }
